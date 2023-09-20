b4b80fd7bf27e233fa2e63e4d39eac9ec88ee3a5
KBCHANUKAJ <chanukakbc@gmail.com>
2023-09-20 13:06:08 +0530

    Spring Project

---
+++ Work_Shop_Backend/src/main/java/com/ictec/work_shop_backend/WorkShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ictec.work_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShopBackendApplication.class, args);
+    }
+
+}
---
+++ Work_Shop_Backend/src/test/java/com/ictec/work_shop_backend/WorkShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictec.work_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
